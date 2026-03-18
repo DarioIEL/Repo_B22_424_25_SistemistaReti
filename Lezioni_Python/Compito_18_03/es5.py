8dcd60fe528bd3e4b822d9c989ebb5cecd183301
= <=>
2026-03-18 13:55:48 +0100

    updel

--- Lezioni_Python/Compito_18_03/es5.py
+++
@@ -1,9 +0,0 @@
-#Chiedi di inserire qualcosa
-nome = input("Come ti chiami ?")
-eta = int( input("Quanti anni hai ?") )
-
-if eta <= 0 or eta > 120:
-    print("Età non valida")
-else:
-    etaNuova = eta + 5
-    print(f"Ciao{nome}, tra 5 anni avrai {etaNuova}")
